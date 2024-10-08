329a7d66607f2404fc1f0e3b99a3e3e9669c6832
Joel Nanai <joel.nanai@bluewin.ch>
2024-10-08 20:58:50 +0200

    Structural changes to the config_manager and the loading process for the functions, making them smaller and more efficient.

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import argparse
-from src.config_manager import load_config
 from src.modes import run_camera_mode, run_image_mode
 
 
 def main():
-
-    config = load_config()
-
-    # Set up argument parser for mode selection
     parser = argparse.ArgumentParser(description="Shape and Color Detection")
-    parser.add_argument('--mode', required=True, choices=['CAMERA', 'IMAGE'],
-                        help="Choose the mode of operation: CAMERA or IMAGE")
+    parser.add_argument('--mode', type=str, choices=['CAMERA', 'IMAGE'], required=True,
+                        help="Mode to run: 'CAMERA' for live feed, 'IMAGE' for static image analysis")
+
     args = parser.parse_args()
 
     if args.mode == 'CAMERA':
-        run_camera_mode(config)
+        run_camera_mode()
     elif args.mode == 'IMAGE':
-        run_image_mode(config)
+        run_image_mode()
 
 
 if __name__ == "__main__":
--- src/modes.py
+++ src/modes.py
@@ -3,30 +3,39 @@ import os
 from src.ShapeAndColorDetection import process_frame
 from src.logger import DataLogger
 from src.visualize_shapes import visualize_shapes
+from src.config_manager import load_shape_params, load_camera_params, load_color_ranges, load_config
 
 
-def run_camera_mode(config):
-    camera_index = int(config['CAMERA']['camera_index']); window_size = config['CAMERA']['window_size'].split('x')
-    fps = int(config['CAMERA']['fps']); log_interval = float(config['DEFAULT']['log_interval'])
+def set_camera(camera_params):
+    cap = cv2.VideoCapture(camera_params['camera_index'])
+    cap.set(cv2.CAP_PROP_FPS, camera_params['fps'])
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, int(camera_params['window_size'][0]))
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(camera_params['window_size'][1]))
+    return cap
+
+
+def initialize_logger(config):
     log_file = os.path.join(config['DEFAULT']['log_folder'], 'data_log.csv')
+    log_interval = float(config['DEFAULT']['log_interval'])
+    return DataLogger(log_file, log_interval=log_interval)
 
-    logger = DataLogger(log_file, log_interval=log_interval)
 
-    cap = cv2.VideoCapture(camera_index)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, int(window_size[0])); cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(window_size[1]))
-    cap.set(cv2.CAP_PROP_FPS, fps)
+def run_camera_mode():
+    config = load_config(); shape_params = load_shape_params(config)
+    camera_params = load_camera_params(config); color_ranges = load_color_ranges(config, 'CAMERA')
 
-    frame_count = 0; process_interval = 5  # Process every fifth frame
+    logger = initialize_logger(config)
+    cap = set_camera(camera_params)
 
     while True:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) + 5)
+
         ret, frame = cap.read()
         if not ret:
             break
-        if frame_count % process_interval == 0:  # Only process every fifth frame
-            shapes = process_frame(frame, config, mode='CAMERA')
-            frame_with_shapes = visualize_shapes(frame, shapes, logger)
-            cv2.imshow("Camera Feed", frame_with_shapes)
-        frame_count = (frame_count + 1) % process_interval  # Reset counter after reaching process_interval
+        shapes = process_frame(frame, shape_params, color_ranges)
+        frame_with_shapes = visualize_shapes(frame, shapes, logger)
+        cv2.imshow("Camera Feed", frame_with_shapes)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -34,22 +43,22 @@ def run_camera_mode(config):
     cv2.destroyAllWindows()
 
 
+def run_image_mode():
+    config = load_config()
+    shape_params = load_shape_params(config)
+    color_ranges = load_color_ranges(config, 'IMAGE')
 
+    logger = initialize_logger(config)
 
-def run_image_mode(config):
-    image_directory = config['IMAGE']['image_directory']
-    log_file = os.path.join(config['DEFAULT']['log_folder'], 'data_log.csv')
-
-    logger = DataLogger(log_file)
-
-    for filename in os.listdir(image_directory):
+    for filename in os.listdir(config['IMAGE']['image_directory']):
         if filename.endswith(('.png', '.jpg', '.jpeg')):
-            image_path = os.path.join(image_directory, filename)
+            image_path = os.path.join(config['IMAGE']['image_directory'], filename)
             image = cv2.imread(image_path)
             if image is None:
                 print(f"Error loading image: {filename}")
                 continue
-            shapes = process_frame(image, config, mode='IMAGE')
+
+            shapes = process_frame(image, shape_params, color_ranges)
             image_with_shapes = visualize_shapes(image, shapes, logger)
             cv2.imshow(f"Image: {filename}", image_with_shapes)
             cv2.waitKey(0)
