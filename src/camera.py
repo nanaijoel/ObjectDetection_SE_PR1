9d0977f182ac6dd82309ccdba5ecf7c4b1588c58
Joel Nanai <joel.nanai@bluewin.ch>
2024-10-17 16:50:14 +0200

    Added a static method to the Camera class to list available camera IDs on the user's system. Also updated the README file accordingly.

--- src/camera.py
+++ src/camera.py
@@ -36,3 +36,22 @@ class Camera:
     @staticmethod
     def close_window():
         cv2.destroyAllWindows()
+
+    @staticmethod
+    def list_webcams():
+        index = 0
+        arr = []
+        while True:
+            cap = cv2.VideoCapture(index)
+            if not cap.read()[0]:
+                break
+            else:
+                arr.append(index)
+            cap.release()
+            index += 1
+        return arr
+
+
+if __name__ == "__main__":
+    available_cameras = Camera.list_webcams()
+    print("Available camera IDs:", available_cameras)
